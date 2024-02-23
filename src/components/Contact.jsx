9ee83dfb8994d0a4dbdc1240090c8d292c1eaa3c
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2024-02-23 03:56:06 -0300

    feat: contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,104 @@
+import "../styles/form.css";
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { TfiEmail } from "react-icons/tfi";
+
+const Contact = () => {
+  const [email, setEmail] = useState("");
+  const [assunto, setAssunto] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (email === "" || assunto === "" || mensagem === "") {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    const templateParams = {
+      from_name: assunto,
+      message: mensagem,
+      email: email,
+    };
+    emailjs
+      .send(
+        "service_75euycg",
+        "template_wr4ljjd",
+        templateParams,
+        "YN6R2IIqm_162n00U"
+      )
+      .then(
+        (response) => {
+          console.log("mensagem enviada", response.status, response.text);
+          setAssunto("");
+          setEmail("");
+          setMensagem("");
+        },
+        (err) => {
+          console.log("erro:", err);
+        }
+      );
+  };
+
+  return (
+    <section id="contact">
+      <div className="titulo contactme">Vamos conversar?</div>
+      <form onSubmit={handleSubmit} className="form">
+        <label htmlFor="email">Email:</label>
+        <input
+          placeholder="e-mail"
+          class="inputclass"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="assunto">Nome:</label>
+        <input
+          placeholder="Nome"
+          class="inputclass"
+          type="text"
+          id="assunto"
+          value={assunto}
+          onChange={(e) => setAssunto(e.target.value)}
+          required
+        />
+        <label htmlFor="mensagem">Mensagem:</label>
+        <textarea
+          placeholder="Mensagem"
+          class="inputclass"
+          id="mensagem"
+          value={mensagem}
+          onChange={(e) => setMensagem(e.target.value)}
+          required
+        ></textarea>
+        <button className="buttonenv" type="submit">
+          Enviar
+        </button>
+      </form>
+      <div class="fim">
+        <h2 class="textfim">Contatos e Redes Sociais</h2>
+        <div class="iconerede">
+          <div class="redesocial">
+            <FaGithub className="icon" />
+            <a class="linksocial">github.com/victorsota</a>
+          </div>
+          <div class="redesocial">
+            <FaLinkedin className="icon" />
+            <a class="linksocial">linkedin.com/in/victor-gabriel-vieira/</a>
+          </div>
+          <div class="redesocial">
+            <TfiEmail className="icon" />
+            <a class="linksocial">victorgabrielfrancisco@gmail.com</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
